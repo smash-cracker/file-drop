db44d61d047aa79239d24a99bdfaad400cb4cc80
smash-cracker <dennythomas069@gmail.com>
2026-01-04 10:30:08 +0530

    feat: add cardds

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,36 @@ import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   return (
-    <main>
-      <h1 className="min-h-[calc(100vh-4rem)] flex items-center justify-center">
-        Template Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
-        eaque minus consequuntur exercitationem corporis! Consectetur
-        perspiciatis quas, quis necessitatibus eos quasi nihil illo ipsum
-        tempore nostrum tenetur impedit unde earum.
-      </h1>
+    <main className="min-h-[calc(100vh-4rem)]  flex items-center justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-3xl px-4">
+        {/* Send Card */}
+        <Card>
+          <CardHeader>
+            <CardTitle>Send File</CardTitle>
+            <CardDescription>
+              Share a file directly with another device
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Link href="/send">
+              <Button className="w-full">Send</Button>
+            </Link>
+          </CardContent>
+        </Card>
+
+        {/* Receive Card */}
+        <Card>
+          <CardHeader>
+            <CardTitle>Receive File</CardTitle>
+            <CardDescription>Receive a file using a room code</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Link href="/receive">
+              <Button className="w-full">Receive</Button>
+            </Link>
+          </CardContent>
+        </Card>
+      </div>
     </main>
   );
 }
