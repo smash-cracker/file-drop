761a374faf77d52c449393f31062ded124f2ef1f
smash-cracker <dennythomas069@gmail.com>
2026-08-25 10:25:53 +0530

    feat: add websocket support

--- main.go
+++ main.go
@@ -4,9 +4,17 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
 var tmpl = template.Must(template.ParseFiles("templates/index.html"))
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	err := tmpl.Execute(w, nil)
@@ -15,8 +23,35 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func websocketHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("WebSocket upgrade failed:", err)
+		return
+	}
+	defer conn.Close()
+
+	log.Println("Browser connected via WebSocket")
+
+	for {
+		messageType, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Browser disconnected")
+			return
+		}
+
+		log.Printf("Received: %s\n", message)
+		err = conn.WriteMessage(messageType, message)
+		if err != nil {
+			log.Println("Failed to send message:", err)
+			return
+		}
+	}
+}
+
 func main() {
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/ws", websocketHandler)
 
 	static := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", static))
--- static/app.js
+++ static/app.js
@@ -1,1 +1,19 @@
-console.log("File Share frontend loaded");
+const socket = new WebSocket(`ws://${window.location.host}/ws`);
+
+socket.onopen = () => {
+    console.log("WebSocket connected");
+
+    socket.send("Hello from browser");
+};
+
+socket.onmessage = (event) => {
+    console.log("Server replied:", event.data);
+};
+
+socket.onclose = () => {
+    console.log("WebSocket disconnected");
+};
+
+socket.onerror = (error) => {
+    console.error("WebSocket error:", error);
+};
